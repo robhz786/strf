6be87bbb2947cd641126e8b63ea492a46228094a
Roberto Hinz <robhz786@gmail.com>
2014-07-05 14:19:21 -0300

    refactor performance code.

--- performance/versus_sprintf.cpp
+++ performance/versus_sprintf.cpp
@@ -11,8 +11,7 @@
 
 static char buff[1000000];
 
-static long common_multiplier = 200000;
-static long number_of_repetitions = 1000 * common_multiplier;
+static long common_multiplier = 500000;
 
 using boost::timer::cpu_times;
 using boost::timer::cpu_timer;
@@ -69,11 +68,16 @@ private:
       lman.loop_condition();                                               \
       lman.advance_iteration())
 
-#define MEASURE_TIME1 MEASURE_TIME_OF_LOOP(time1, number_of_repetitions)
-#define MEASURE_TIME2 MEASURE_TIME_OF_LOOP(time2, number_of_repetitions)
-#define MEASURE_TIME3 MEASURE_TIME_OF_LOOP(time3, number_of_repetitions)
-#define MEASURE_TIME4 MEASURE_TIME_OF_LOOP(time4, number_of_repetitions)
-#define MEASURE_TIME5 MEASURE_TIME_OF_LOOP(time5, number_of_repetitions)
+#define MEASURE_TIME(clock_resolution, output_variable) \
+MEASURE_TIME_OF_LOOP(output_variable, clock_resolution* common_multiplier)
+
+#define MEASURE_TIME1(clock_resolution) MEASURE_TIME(clock_resolution, time1)
+#define MEASURE_TIME2(clock_resolution) MEASURE_TIME(clock_resolution, time2)
+#define MEASURE_TIME3(clock_resolution) MEASURE_TIME(clock_resolution, time3)
+#define MEASURE_TIME4(clock_resolution) MEASURE_TIME(clock_resolution, time4)
+#define MEASURE_TIME5(clock_resolution) MEASURE_TIME(clock_resolution, time5)
+
+
 
 
 void write_hello(char* out)
@@ -89,66 +93,100 @@ void write_hello(char* out)
 
 int main()
 {
-  number_of_repetitions = 1000 * common_multiplier;
 
   {
-    MEASURE_TIME1
-    {
-      sprintf(buff, "hello"); 
-    }
-    MEASURE_TIME2
-    {
-      sprintf(buff, "%s", "hello"); 
-    }
-    MEASURE_TIME3
-    {
-      strcpy(buff, "hello"); 
-    }
-    MEASURE_TIME4
+    MEASURE_TIME1(100)
     {
       write_hello(buff);
     }
-    number_of_repetitions = 100 * common_multiplier;
-    MEASURE_TIME5
+    MEASURE_TIME2(1000)
     {
       write(buff, boost::listf{"hello"});
     }
-    std::cout << time1 << "  " 
-              << time2 << "  " 
-              << time3 << "  " 
-              << time4 << "  " 
-              << time5 << std::endl;
+    MEASURE_TIME3(1000)
+    {
+      sprintf(buff, "hello"); 
+    }
+    MEASURE_TIME4(1000)
+    {
+      sprintf(buff, "%s", "hello"); 
+    }
+    MEASURE_TIME5(500)
+    {
+      strcpy(buff, "hello"); 
+    }
+    std::cout << time1 << " / "  << std::flush
+              << time2 << " / "  << std::flush
+              << time3 << " / "  << std::flush
+              << time4 << " / "  << std::flush
+              << time5 << " / "  << std::flush 
+              << std::endl;
   }
 
-
-
-  number_of_repetitions = 10 * common_multiplier;
   {
-    std::string hello("hello");
-    std::string fmt("%s");
-    const char* hello_cstr = hello.c_str();
-    const char* fmt_cstr = fmt.c_str();
+
+    std::string std_string_hello("hello");
+    const char* hello = std_string_hello.c_str();
+
+    std::string std_string_fmt("%s");
+    const char* fmt = std_string_fmt.c_str();
 
 
-    MEASURE_TIME1
+    MEASURE_TIME1(100)
     {
-      sprintf(buff, hello_cstr); 
+      write(buff, boost::listf{hello});
     }
-    MEASURE_TIME2
+    MEASURE_TIME2(25)
     {
-      sprintf(buff, fmt_cstr, hello_cstr); 
+      sprintf(buff, hello); 
     }
-    MEASURE_TIME3
+    MEASURE_TIME3(200)
     {
-      write(buff, boost::listf{hello_cstr});
+      sprintf(buff, "%s", hello); 
     }
-    std::cout << time1 << "  " << time2 << "  " << time3 << std::endl;
+    MEASURE_TIME4(10)
+    {
+      sprintf(buff, fmt, hello); 
+    }
+    MEASURE_TIME5(200)
+    {
+      strcpy(buff, hello); 
+    }
+    std::cout << time1 << " / "  << std::flush
+              << time2 << " / "  << std::flush
+              << time3 << " / "  << std::flush
+              << time4 << " / "  << std::flush
+              << time5 << " / "  << std::flush
+              << std::endl;
   }
+  {
+    int x = 12345;
+    std::string std_string_fmt("%d");
+    const char* fmt = std_string_fmt.c_str();
 
+    MEASURE_TIME1(40)
+    {
+      write(buff, boost::listf{x});
+    }
+    MEASURE_TIME2(100)
+    {
+      sprintf(buff, "%d", 12345);
+    }
+    MEASURE_TIME3(10)
+    {
+      sprintf(buff, "%d", x); 
+    }
+    MEASURE_TIME4(10)
+    {
+      sprintf(buff, fmt, x); 
+    }
 
-
-
-
+    std::cout << time1 << " / "  << std::flush
+              << time2 << " / "  << std::flush
+              << time3 << " / "  << std::flush
+              << time4 << " / "  << std::flush
+              << std::endl;
+  }
 
   return 1;
 }
