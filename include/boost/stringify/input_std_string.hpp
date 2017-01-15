dd113333fedce10ddbfc0e021feda033fc9d9de8
Roberto Hinz <robhz786@gmail.com>
2017-01-15 19:06:22 -0200

    rename test files

--- include/boost/stringify/input_std_string.hpp
+++ include/boost/stringify/input_std_string.hpp
@@ -6,7 +6,7 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 
 #include <string>
-#include <boost/stringify/stringifier.hpp>
+#include <boost/stringify/input_char_ptr.hpp>
 
 namespace boost {
 namespace stringify{
@@ -19,40 +19,87 @@ class std_string_stringifier
     typedef boost::stringify::stringifier<CharT, Output, Formatting> base;
 
 public:
-
+    typedef boost::stringify::detail::string_arg_formatting arg_format_type;
     typedef const std::basic_string<CharT, Traits>& input_type;
     typedef CharT char_type;
     typedef Output output_type;
     typedef Formatting ftuple_type;
 
-    std_string_stringifier
+   std_string_stringifier
         ( const Formatting& fmt
-        , const std::basic_string<CharT, Traits>& str
+        , const input_type& str
+        , arg_format_type argf
         ) noexcept
         : m_fmt(fmt)
         , m_str(str)
+        , m_padding_width(padding_width(argf.get_width<input_type>(fmt)))
+        , m_alignment(boost::stringify::get_alignment<input_type>(fmt, argf.m_flags))
+    {
+    }
+    
+    std_string_stringifier(const Formatting& fmt, const input_type& str) noexcept
+        : m_fmt(fmt)
+        , m_str(str)
+        , m_padding_width
+          (padding_width(boost::stringify::get_width<input_type>(fmt)))
+        , m_alignment(boost::stringify::get_alignment<input_type>(fmt))
     {
     }
 
     virtual std::size_t length() const noexcept override
     {
+        if (m_padding_width > 0)
+        {
+            return m_str.length() + 
+                boost::stringify::fill_length<CharT, input_type>
+                (m_padding_width, m_fmt);
+        }
         return m_str.length();
     }
 
     void write(Output& out) const noexcept(base::noexcept_output) override
     {
-        if(m_str)
+        if (m_padding_width > 0)
         {
-            out.put(m_str.c_str(), m_str.length());
+            if(m_alignment == boost::stringify::alignment::left)
+            {
+                out.put(&m_str[0], m_str.length());
+                write_fill(out);
+            }
+            else
+            {
+                write_fill(out);
+                out.put(&m_str[0], m_str.length());
+            }
+        }
+        else
+        {
+            out.put(&m_str[0], m_str.length());
         }
     }
-    
+
+
 private:
-    
+
     const Formatting& m_fmt;
-    const std::basic_string<CharT, Traits>& m_str;
+    const input_type& m_str;
+    const width_t m_padding_width;
+    boost::stringify::alignment m_alignment;
     
+    void write_fill(Output& out) const noexcept(base::noexcept_output)
+    {
+        boost::stringify::write_fill<CharT, input_type>
+                (m_padding_width, out, m_fmt);
+    }
+    
+    width_t padding_width(width_t total_width) const noexcept
+    {
+        return
+            boost::stringify::get_width_calculator<input_type>(m_fmt)
+            .remaining_width(total_width, &m_str[0], m_str.length());
+    }
 };
+
 
 template <typename CharIn, typename CharTraits>
 struct std_string_input_traits
