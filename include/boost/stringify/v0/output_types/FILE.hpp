e4e70aa0e136f5def94bbbb6e33832b1b9da4d4a
Roberto Hinz <robhz786@gmail.com>
2017-09-16 21:04:34 -0300

    redesign write_to(FILE*) as template

--- include/boost/stringify/v0/output_types/FILE.hpp
+++ include/boost/stringify/v0/output_types/FILE.hpp
@@ -12,12 +12,13 @@ BOOST_STRINGIFY_V0_NAMESPACE_BEGIN
 
 namespace detail {
 
-class narrow_file_writer: public output_writer<char>
+template <typename CharT>
+class narrow_file_writer: public output_writer<CharT>
 {
 
 public:
 
-    using char_type = char;
+    using char_type = CharT;
 
     narrow_file_writer(std::FILE* file, std::size_t* count)
         : m_file(file)
@@ -29,31 +30,89 @@ public:
         }
     }
 
-    void set_error(std::error_code err) override;
+    void set_error(std::error_code err) override
+    {
+        if(err && !m_err)
+        {
+            m_err = err;
+        }
+    }
 
-    bool good() const override;
+    bool good() const override
+    {
+        return ! m_err;
+    }
 
-    void put(const char_type* str, std::size_t count) override;
+    void put(const char_type* str, std::size_t count) override
+    {
+        if ( ! m_err )
+        {
+            std::size_t count_inc = std::fwrite
+                (str, sizeof(char_type), count, m_file);
 
-    void put(char_type ch) override;
+            if(m_count != nullptr)
+            {
+                *m_count += count_inc;
+            }
+            if (count != count_inc)
+            {
+                m_err = std::error_code{errno, std::generic_category()};
+            }
+        }
+    }
+
+    void put(char_type ch) override
+    {
+        if ( ! m_err)
+        {
+            do_put(ch);
+        }
+    }
 
     void repeat
-        ( char_type ch1
+        ( char_type ch
         , std::size_t count
-        ) override;
+        ) override
+    {
+        if( ! m_err)
+        {
+            while(count > 0 && do_put(ch))
+            {
+                --count;
+            }
+        }
+    }
 
     void repeat
         ( char_type ch1
         , char_type ch2
         , std::size_t count
-        ) override;
+        ) override
+    {
+        if( ! m_err)
+        {
+            while(count > 0 && do_put(ch1) && do_put(ch2))
+            {
+                --count;
+            }
+        }
+    }
 
     void repeat
         ( char_type ch1
         , char_type ch2
         , char_type ch3
         , std::size_t count
-        ) override;
+        ) override
+    {
+        if( ! m_err)
+        {
+            while(count > 0 && do_put(ch1) && do_put(ch2) && do_put(ch3))
+            {
+                --count;
+            }
+        }
+    }
 
     void repeat
         ( char_type ch1
@@ -61,13 +120,47 @@ public:
         , char_type ch3
         , char_type ch4
         , std::size_t count
-        ) override;
+        ) override
+    {
+        if( ! m_err)
+        {
+            while
+                ( count > 0
+                  && do_put(ch1)
+                  && do_put(ch2)
+                  && do_put(ch3)
+                  && do_put(ch4)
+                )
+            {
+                --count;
+            }
+        }    
+    }
 
-    std::error_code finish();
+    std::error_code finish()
+    {
+        return m_err;
+    }
+
 
 private:
 
-    bool do_put(char_type ch);
+    bool do_put(char_type ch)
+    {
+        if(0 == std::fwrite(&ch, sizeof(char_type), 1, m_file))
+        {
+            m_err = std::error_code{errno, std::generic_category()};
+            return false;
+        }
+        else
+        {
+            if(m_count != nullptr)
+            {
+                ++ *m_count;
+            }
+            return true;
+        }
+    }
 
     std::FILE* m_file;
     std::size_t* m_count = nullptr;
@@ -138,138 +231,16 @@ private:
 
 };
 
+#if defined(BOOST_STRINGIFY_NOT_HEADER_ONLY)
+
+BOOST_STRINGIFY_EXPLICIT_TEMPLATE class narrow_file_writer<char>;
+BOOST_STRINGIFY_EXPLICIT_TEMPLATE class narrow_file_writer<char16_t>;
+BOOST_STRINGIFY_EXPLICIT_TEMPLATE class narrow_file_writer<char32_t>;
+BOOST_STRINGIFY_EXPLICIT_TEMPLATE class narrow_file_writer<wchar_t>;
+
+#endif
 
 #if ! defined(BOOST_STRINGIFY_OMIT_IMPL)
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::set_error(std::error_code err)
-{
-    if(err && !m_err)
-    {
-        m_err = err;
-    }
-}
-
-BOOST_STRINGIFY_INLINE bool narrow_file_writer::good() const
-{
-    return ! m_err;
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::put
-    ( const char_type* str
-    , std::size_t count
-    )
-{
-    if ( ! m_err )
-    {
-        std::size_t count_inc = std::fwrite(str, 1, count, m_file);
-        if(m_count != nullptr)
-        {
-            *m_count += count_inc;
-        }
-        if (count != count_inc)
-        {
-            m_err = std::error_code{errno, std::generic_category()};
-        }
-    }
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::put(char_type ch)
-{
-    if ( ! m_err)
-    {
-        do_put(ch);
-    }
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::repeat
-    ( char_type ch
-    , std::size_t count
-    )
-{
-    if( ! m_err)
-    {
-        while(count > 0 && do_put(ch))
-        {
-            --count;
-        }
-    }
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::repeat
-    ( char_type ch1
-    , char_type ch2
-    , std::size_t count
-    )
-{
-    if( ! m_err)
-    {
-        while(count > 0 && do_put(ch1) && do_put(ch2))
-        {
-            --count;
-        }
-    }
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::repeat
-    ( char_type ch1
-    , char_type ch2
-    , char_type ch3
-    , std::size_t count
-    )
-{
-    if( ! m_err)
-    {
-        while(count > 0 && do_put(ch1) && do_put(ch2) && do_put(ch3))
-        {
-            --count;
-        }
-    }
-}
-
-BOOST_STRINGIFY_INLINE void narrow_file_writer::repeat
-    ( char_type ch1
-    , char_type ch2
-    , char_type ch3
-    , char_type ch4
-    , std::size_t count
-    )
-{
-    if( ! m_err)
-    {
-        while
-            ( count > 0
-           && do_put(ch1)
-           && do_put(ch2)
-           && do_put(ch3)
-           && do_put(ch4)
-            )
-        {
-            --count;
-        }
-    }    
-}
-
-BOOST_STRINGIFY_INLINE std::error_code narrow_file_writer::finish()
-{
-    return m_err;
-}
-
-BOOST_STRINGIFY_INLINE bool narrow_file_writer::do_put(char_type ch)
-{
-    if(std::fputc(ch, m_file) == EOF)
-    {
-        m_err = std::error_code{errno, std::generic_category()};
-        return false;
-    }
-    else
-    {
-        if(m_count != nullptr)
-        {
-            ++ *m_count;
-        }
-        return true;
-    }
-}
 
 BOOST_STRINGIFY_INLINE void wide_file_writer::set_error(std::error_code err)
 {
@@ -400,9 +371,10 @@ BOOST_STRINGIFY_INLINE bool wide_file_writer::do_put(char_type ch)
 
 } // namespace detail
 
+template <typename CharT = char>
 inline auto write_to(std::FILE* destination, std::size_t* count = nullptr)
 {
-    using writer = stringify::v0::detail::narrow_file_writer;
+    using writer = stringify::v0::detail::narrow_file_writer<CharT>;
     return stringify::v0::make_args_handler<writer>(destination, count);
 }
 
--- test/v0/output_FILE_test.cpp
+++ test/v0/output_FILE_test.cpp
@@ -5,168 +5,282 @@
 #define  _CRT_SECURE_NO_WARNINGS
 
 #include <boost/detail/lightweight_test.hpp>
+#include <boost/stringify.hpp>
+#include <boost/config.hpp>
 #include "test_utils.hpp"
 #include "error_code_emitter_arg.hpp"
 #include "exception_thrower_arg.hpp"
-#include <boost/stringify.hpp>
-#include <fstream>
+
 
 namespace strf = boost::stringify::v0;
+
+std::wstring read_wfile(std::FILE* file)
+{
+    std::wstring result;
+    wint_t ch = fgetwc(file);
+    while(ch != WEOF)
+    {
+        result += static_cast<wchar_t>(ch);
+        ch = fgetwc(file);
+    };
+
+    return result;
+}
+
+
+template <typename CharT>
+std::basic_string<CharT> read_file(std::FILE* file)
+{
+    constexpr std::size_t buff_size = 500;
+    CharT buff[buff_size];
+    std::basic_string<CharT> result;
+    std::size_t read_size = 0;
+    do
+    {
+        read_size = std::fread(buff, sizeof(buff[0]), buff_size, file);
+        result.append(buff, read_size);
+    }
+    while(read_size == buff_size);
+
+    return result;
+}
 
 
 template <typename CharT>
 std::basic_string<CharT> read_file(const char* filename)
 {
-    constexpr std::size_t buff_size = 500;
-    CharT buff[buff_size];
     std::basic_string<CharT> result;
+
+    std::FILE* file = nullptr;
+    try
     {
-        std::FILE* file = std::fopen(filename, "r");
-        std::size_t read_size = 0;
-        do
+        file = filename == nullptr ? nullptr : std::fopen(filename, "r");
+        if(file != nullptr)
         {
-            read_size = std::fread(buff, sizeof(buff[0]), buff_size, file);
-            result.append(buff, read_size);
+            result = read_file<CharT>(file);
         }
-        while(read_size == buff_size);
+    }
+    catch(...)
+    {
+    }
+    if (file != nullptr)
+    {
         fclose(file);
     }
+    
     return result;
 }
 
+
+template <typename CharT>
+void basic_test__narrow()
+{
+    std::size_t result_length = 1000;
+    std::basic_string<CharT> expected;
+    std::basic_string<CharT> result;
+    std::error_code err = std::make_error_code(std::errc::operation_canceled);
+    std::FILE* file = nullptr;
+    
+    try 
+    {   
+        file = std::tmpfile();
+        err = use_all_writing_function_of_output_writer
+            ( strf::write_to<CharT>(file, &result_length)
+            , expected );
+
+        std::rewind(file);
+        result = read_file<CharT>(file);
+    }
+    catch(...)
+    {
+    }
+    
+    if(file != nullptr)
+    {
+        std::fclose(file);
+    }
+
+    BOOST_TEST(!err);
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+
+template <typename CharT>
+void error_code_test__narrow()
+{
+    std::size_t result_length = 1000;
+    std::basic_string<CharT> expected;
+    expected.push_back('a');
+    expected.push_back('b');
+    expected.push_back('c');
+
+    std::basic_string<CharT> result;
+    std::error_code err {};
+    std::FILE* file = nullptr;
+    
+    try 
+    {   
+        file = std::tmpfile();
+        err = strf::write_to<CharT>(file, &result_length)
+            [{U'a', U'b', U'c', error_code_emitter_arg, U'x', U'y', U'z'}];
+
+        std::rewind(file);
+        result = read_file<CharT>(file);
+    }
+    catch(...)
+    {
+    }
+    
+    if(file != nullptr)
+    {
+        std::fclose(file);
+    }
+
+    BOOST_TEST(err == std::errc::invalid_argument);
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+
+template <typename CharT>
+void exception_thrown_test__narrow()
+{
+    std::size_t result_length = 1000;
+    std::basic_string<CharT> expected;
+    expected.push_back('a');
+    expected.push_back('b');
+    expected.push_back('c');
+
+    std::basic_string<CharT> result;
+    if(std::FILE* file = std::tmpfile())
+    {
+        try 
+        {                         
+            strf::write_to<CharT>(file, &result_length)
+                [{U'a', U'b', U'c', exception_thrower_arg, U'x', U'y', U'z'}];
+
+        }
+        catch(...)
+        {
+        }
+        std::rewind(file);
+        result = read_file<CharT>(file);
+        std::fclose(file);
+    }
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+
+void basic_test__wide()
+{
+    std::size_t result_length = 1000;
+    std::wstring expected = L"abcdyyyyz";
+    std::wstring result;
+    std::error_code err = std::make_error_code(std::errc::operation_canceled);
+    std::FILE* file = nullptr;
+    
+    try 
+    {
+        file = std::tmpfile();
+        err = strf::wwrite_to(file, &result_length)
+        [{
+            L"abcd", {L'x', {"", 0}}, {L'y', {"", 4}}, L'z'
+        }];
+        std::rewind(file);
+        result = read_wfile(file);
+    }
+    catch(...)
+    {
+    }
+    
+    if(file != nullptr)
+    {
+        std::fclose(file);
+    }
+
+    BOOST_TEST(!err);
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+
+void error_code_test__wide()
+{
+    std::size_t result_length = 1000;
+    std::wstring expected = L"abc";
+    std::wstring result;
+    std::error_code err {};
+    std::FILE* file = nullptr;
+    
+    try 
+    {   
+        file = std::tmpfile();
+        err = strf::wwrite_to(file, &result_length)
+            [{L"abc", error_code_emitter_arg, L"xyz"}];
+
+        std::rewind(file);
+        result = read_wfile(file);
+    }
+    catch(...)
+    {
+    }
+    
+    if(file != nullptr)
+    {
+        std::fclose(file);
+    }
+
+    BOOST_TEST(err == std::errc::invalid_argument);
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+void exception_thrown_test__wide()
+{
+    std::size_t result_length = 1000;
+    std::wstring expected = L"abc";
+    std::wstring result;
+    if (std::FILE* file = std::tmpfile())
+    {
+        try 
+        {   
+            strf::wwrite_to(file, &result_length)
+                [{L"abc", exception_thrower_arg, L"xyz"}];
+
+        }
+        catch(...)
+        {
+        }
+        std::rewind(file);
+        result = read_wfile(file);
+    }
+    BOOST_TEST(expected.length() == result_length);
+    BOOST_TEST(expected == result);
+}
+
+
+
 int main()
 {
-    
-    {   // narrow char
-        
-        const char* filename = "stringify_tmptestfile_n.tmp";
-        std::string expected;
-        std::size_t result_length = 1000;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
+    basic_test__narrow<char>();
+    basic_test__narrow<char16_t>();
+    basic_test__narrow<char32_t>();
+    basic_test__narrow<wchar_t>();
+    basic_test__wide();
 
-            std::error_code err = use_all_writing_function_of_output_writer
-                ( strf::write_to(file, &result_length)
-                , expected );
+    error_code_test__narrow<char>();
+    error_code_test__narrow<char16_t>();
+    error_code_test__narrow<char32_t>();
+    error_code_test__narrow<wchar_t>();
+    error_code_test__wide();
 
-            fclose(file);
-            BOOST_TEST(!err);
-        }
-        std::string result = read_file<char>(filename);
-        
-        BOOST_TEST(expected.length() == result_length);
-        BOOST_TEST(expected == result);
-    }
-    
-    {   // wide char
-        const char* filename = "stringify_tmptestfile_.tmp";
-        std::string expected = "abcdyyyyyz";
-        std::size_t result_length = 1000;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
-
-            std::error_code err = strf::wwrite_to(file, &result_length)
-            [{
-                L"abcd", {L'x', {"", 0}}, {L'y', {"", 5}}, L'z'
-            }];
-
-
-            fclose(file);
-            BOOST_TEST(!err);
-        }
-
-        
-        std::string result = read_file<char>(filename);
-        BOOST_TEST(expected.length() == result_length);
-        BOOST_TEST(expected == result);
-    }
-
-    {   // Test set_error / narrow char
-
-        const char* filename = "stringify_tmptestfile.tmp";
-        std::size_t result_length = 1000;
-        std::error_code err_code;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
-            err_code = strf::write_to(file, &result_length)
-                [{"abcd", error_code_emitter_arg, "lkjlj"}];
-            fclose(file);
-        }
-
-        std::string result = read_file<char>(filename);
-        BOOST_TEST(err_code == std::errc::invalid_argument);
-        BOOST_TEST(result == "abcd");
-        BOOST_TEST(result_length == 4);
-    }
-
-    {   // Test set_error / wide char
-       
-        const char* filename = "stringify_tmptestfile.tmp";
-        std::size_t result_length = 1000;
-        std::error_code err_code;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
-
-            err_code = strf::wwrite_to(file, &result_length)
-                [{L"abcd", error_code_emitter_arg, L"lkjlj"}];
-            fclose(file);
-        }
-
-        std::string result = read_file<char>(filename);
-        BOOST_TEST(err_code == std::errc::invalid_argument);
-        BOOST_TEST(result == "abcd");
-        BOOST_TEST(result_length == 4);
-    }
-
-    {   // When exception is thrown / narrow char
-
-        const char* filename = "stringify_tmptestfile.tmp";
-        std::size_t result_length = 1000;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
-            try
-            {
-                strf::write_to(file, &result_length)
-                    [{"abcd", exception_thrower_arg, "lkjlj"}];
-            }
-            catch(...)
-            {
-            }
-            fclose(file);
-        }
-
-        std::string result = read_file<char>(filename);
-        BOOST_TEST(result == "abcd");
-        BOOST_TEST(result_length == 4);
-    }
-
-    {   // When exception is thrown / wide char
-
-        const char* filename = "stringify_tmptestfile.tmp";
-        std::size_t result_length = 1000;
-        {
-            std::remove(filename);
-            std::FILE* file = fopen(filename, "w");
-            try
-            {
-                strf::wwrite_to(file, &result_length)
-                    [{L"abcd", exception_thrower_arg, L"lkjlj"}];
-            }
-            catch(...)
-            {
-            }
-            fclose(file);
-        }
-
-        std::string result = read_file<char>(filename);
-        BOOST_TEST(result == "abcd");
-        BOOST_TEST(result_length == 4);
-    }
+    exception_thrown_test__narrow<char>();
+    exception_thrown_test__narrow<char16_t>();
+    exception_thrown_test__narrow<char32_t>();
+    exception_thrown_test__narrow<wchar_t>();
+    exception_thrown_test__wide();
 
     return boost::report_errors();
 }
