e2abcdbdc13ec6d0b39b24c2e752ba00f61cce4c
Roberto Hinz <robhz786@gmail.com>
2019-11-08 12:17:20 -0300

    update ci files

---
+++ ci/appveyor/build.bat
@@ -0,0 +1,18 @@
+@ECHO ON
+
+mkdir cmake_build
+dir
+cd cmake_build
+cmake -A %ARCH% ^
+    -DCMAKE_CXX_FLAGS=" /W4 /EHsc" ^
+    -DCMAKE_CXX_STANDARD=%CXX_STANDARD% ^
+    -DSTRF_BUILD_TESTS=ON ^
+    -DSTRF_BUILD_EXAMPLES=ON ^
+    -G "%GENERATOR%"  .. ^
+  && cmake --build. --config %CONFIG%
+
+
+
+
+
+    
