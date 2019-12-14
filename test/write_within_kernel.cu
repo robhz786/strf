1a874709d61d49cc416ff6cf4959f45d1c1c0e6d
Eyal Rozenberg <eyalroz@technion.ac.il>
2019-12-14 22:54:31 +0200

    WARNING: Test fails to compile with this commit.
    * Added a test - commented-out - for using `strf::to`, taking a C-style string buffer (`char*` and buffer size), to print within a kernel.
    * ... However - that test didn't compile, for a weird reason, so also added a sanity-check test to do the same on the CPU side. That _also_ doesn't compile: Problems with getting the correct overload of `strf::make_printer`, at `destination.hpp` line 135.

--- test/write_within_kernel.cu
+++ test/write_within_kernel.cu
@@ -27,6 +27,13 @@ __global__ void kernel_using_cstr_writer(strf::cstr_writer::result* write_result
 //  }
 }
 
+//__global__ void kernel_using_cstr_to(char* buffer, std::size_t buffer_size)
+//{
+//  int global_thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+//  auto printer = strf::to(buffer, buffer_size);
+//  printer ( "Hello", ' ', "world, from thread ", global_thread_id );
+//}
+
 
 
 // Ugly, no-good error-checking.
@@ -48,8 +55,8 @@ void test_cstr_writer()
 		strf::cstr_writer::result write_result;
 		char buffer[50];
 	};
-	struct args* device_side_args;
 	const std::size_t buffer_size { std::strlen("Hello world") + 1 }; // Enough for "Hello world" with the trailing '\0'.
+	struct args* device_side_args;
 	ensure_cuda_success(cudaMalloc(&device_side_args, sizeof(struct args)));
 	ensure_cuda_success(cudaMemset(device_side_args, 0, sizeof(struct args)));
 
@@ -65,17 +72,46 @@ void test_cstr_writer()
 	ensure_cuda_success(cudaDeviceSynchronize());
 	args host_side_args;
 	ensure_cuda_success(cudaMemcpy(&host_side_args, device_side_args, sizeof(struct args), cudaMemcpyDeviceToHost));
-//	if (strncmp(host_side_args.write_result.ptr, host_side_args.buffer, buffer_size) == 0) {
-//		BOOST_ERROR("Got the expected string");
-//	}
-//	else {
-//		BOOST_ERROR("Have NOT gotten the expected string");
-//	}
     BOOST_TEST_EQ(host_side_args.write_result.truncated, false);
 	BOOST_TEST_EQ(host_side_args.write_result.ptr, &(device_side_args->buffer[0]) + std::strlen("Hello world"));
 	if (host_side_args.write_result.ptr == &(device_side_args->buffer[0])) {
 		BOOST_TEST_EQ(strncmp(host_side_args.write_result.ptr, host_side_args.buffer, buffer_size), 0);
 	}
+}
+
+/*
+void test_cstr_to()
+{
+	char* device_side_buffer;
+	const std::size_t buffer_size { 100 }; // More than enough for "Hello world from thread XYZ"
+	ensure_cuda_success( cudaMalloc(&device_side_buffer, buffer_size) );
+	ensure_cuda_success( cudaMemset(device_side_buffer, 0, buffer_size) );
+
+	int threads_per_block { 1 };
+	int blocks_in_grid { 1 };
+		// We could theoretically have multiple threads in multiple blocks run this, but
+		// it shouldn't really matter.
+	kernel_using_cstr_to<<<threads_per_block, blocks_in_grid>>>(device_side_buffer, buffer_size);
+	ensure_cuda_success(cudaGetLastError());
+	ensure_cuda_success(cudaDeviceSynchronize());
+	char host_side_buffer[buffer_size];
+	ensure_cuda_success(cudaMemcpy(&host_side_buffer, device_side_buffer, buffer_size , cudaMemcpyDeviceToHost));
+	std::stringstream expected;
+	expected << "Hello" << ' ' << "world, from thread " << 1;
+	BOOST_TEST_EQ(strncmp(host_side_buffer, expected.str().c_str(), buffer_size), 0);
+}
+*/
+
+void cstr_to_sanity_check()
+{
+	const std::size_t buffer_size { 100 }; // More than enough for "Hello world from thread XYZ"
+	char buffer[buffer_size];
+	std::fill_n(buffer, sizeof(buffer), 0);
+    auto print_functor = strf::to(buffer, buffer_size);
+    print_functor ( "Hello", ' ', "world, from thread ", 1 );
+	std::stringstream expected;
+	expected << "Hello" << ' ' << "world, from thread " << 1;
+	BOOST_TEST_EQ(strncmp(buffer, expected.str().c_str(), buffer_size), 0);
 }
 
 
@@ -97,6 +133,8 @@ int main(void)
     }
 	// TODO: Test basic_cstr_writer's with different character types
 	test_cstr_writer();
+	cstr_to_sanity_check();
+//	test_cstr_to();
 
     cudaDeviceReset();
     return boost::report_errors();
