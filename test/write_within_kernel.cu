217279e187a6dedaa71087ff9038b5129d9b77ac
Eyal Rozenberg <eyalroz@technion.ac.il>
2019-12-14 11:19:20 +0200

    Revamped the test for using a cstr writing within a kernel:
    * Checking all CUDA runtime API return values.
    * Skipping the test if no usable CUDA device is present.
    * Instead of printing from the kernel, we copy the write result structure and the buffer with the generated string back to the host, for evaluation.
    * (Test passes...)

--- test/write_within_kernel.cu
+++ test/write_within_kernel.cu
@@ -1,56 +1,103 @@
 #include <stdio.h> // for CUDA's printf
-#include <cstddef> // for CUDA's printf
 
+// #include "lightweight_test_label.hpp"
+#include "test_utils.hpp"
 #include <strf.hpp>
+#include <sstream>
 
-/**
- * CUDA Kernel Device code
- *
- * Computes the vector addition of A and B into C. The 3 vectors
- * are of the same length.
- */
-__global__ void a_test_kernel()
+// Note: There are adaptations of, say, std::span for use with CUDA (= I adapted it...).
+// But we want to avoid dependency clutter here, so let's just stick to the basics.
+
+
+__global__ void kernel_using_cstr_writer(strf::cstr_writer::result* write_result, char* buffer, std::size_t buffer_size)
 {
-  char buf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  int global_thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+//  int global_thread_id = threadIdx.x + blockIdx.x * blockDim.x;
 //  strf::snprintf(buf, "Thread %d says: Hello %s\n", global_thread_id, "world.");
 //  printf("Thread %3d says: Hello %s\n", global_thread_id, "world.");
-  strf::basic_cstr_writer<char> sw(buf);
+  strf::basic_cstr_writer<char> sw(buffer, buffer_size);
   write(sw, "Hello");
-  write(sw, " World.");
-  auto result = sw.finish();
+  write(sw, " world");
+  *write_result = sw.finish();
 
-  if (not result.truncated) {
-	  printf("Thread %03d's finalized string: \"%s\"\n",  global_thread_id, buf);
-  }
-  else {
-	  printf("Thread %03d's truncated string: \"%26s\"\n", global_thread_id, buf);
-  }
-
-//  BOOST_TEST(r.truncated);
-//  BOOST_TEST_EQ(*r.ptr, '\0');
-//  BOOST_TEST_EQ(r.ptr, &buf[7]);
-//  BOOST_TEST_CSTR_EQ(buf, "Hello W");
-//
-//  printf("%s\n", buf);
+//  if (not write_result->truncated) {
+//	  printf("[%s kernel, thread %03d] Finalized string is: \"%s\"\n", __FUNCTION__, global_thread_id, buffer);
+//  }
+//  else {
+//	  printf("[%s kernel, thread %03d] Finalized string is: \"%11s\"\n", __FUNCTION__, global_thread_id, buffer);
+//  }
 }
+
+
+
+// Ugly, no-good error-checking.
+#define ensure_cuda_success(ans) { ensure_cuda_success_((ans), __FILE__, __LINE__); }
+
+inline void ensure_cuda_success_(cudaError_t status, const char *file, int line, bool abort=true)
+{
+  BOOST_TEST_EQ(status, cudaSuccess);
+  if (abort and (status != cudaSuccess)) {
+    BOOST_ERROR(cudaGetErrorString(status));
+    exit(boost::report_errors());
+  }
+}
+
+
+void test_cstr_writer()
+{
+	struct args {
+		strf::cstr_writer::result write_result;
+		char buffer[50];
+	};
+	struct args* device_side_args;
+	const std::size_t buffer_size { std::strlen("Hello world") + 1 }; // Enough for "Hello world" with the trailing '\0'.
+	ensure_cuda_success(cudaMalloc(&device_side_args, sizeof(struct args)));
+	ensure_cuda_success(cudaMemset(device_side_args, 0, sizeof(struct args)));
+
+	int threads_per_block { 1 };
+	int blocks_in_grid { 1 };
+		// We could theoretically have multiple threads in multiple blocks run this, but
+		// it shouldn't really matter.
+	kernel_using_cstr_writer<<<threads_per_block, blocks_in_grid>>>(
+		&(device_side_args->write_result),
+		&(device_side_args->buffer[0]),
+		buffer_size);
+	ensure_cuda_success(cudaGetLastError());
+	ensure_cuda_success(cudaDeviceSynchronize());
+	args host_side_args;
+	ensure_cuda_success(cudaMemcpy(&host_side_args, device_side_args, sizeof(struct args), cudaMemcpyDeviceToHost));
+//	if (strncmp(host_side_args.write_result.ptr, host_side_args.buffer, buffer_size) == 0) {
+//		BOOST_ERROR("Got the expected string");
+//	}
+//	else {
+//		BOOST_ERROR("Have NOT gotten the expected string");
+//	}
+    BOOST_TEST_EQ(host_side_args.write_result.truncated, false);
+	BOOST_TEST_EQ(host_side_args.write_result.ptr, &(device_side_args->buffer[0]) + std::strlen("Hello world"));
+	if (host_side_args.write_result.ptr == &(device_side_args->buffer[0])) {
+		BOOST_TEST_EQ(strncmp(host_side_args.write_result.ptr, host_side_args.buffer, buffer_size), 0);
+	}
+}
+
 
 int main(void)
 {
-    int threads_per_block = 3;
-    int blocks_in_grid = 4;
-//    printf("Launching the CUDA kernel on a grid with %d blocks of %d threads each.\n", blocks_in_grid, threads_per_block);
-    a_test_kernel<<<blocks_in_grid, threads_per_block>>>();
+    auto num_devices { 0 };
+    auto status = cudaGetDeviceCount(&num_devices);
 
-    cudaDeviceSynchronize();
+    BOOST_TEST_EQ(status, cudaSuccess);
+    if (status != cudaSuccess)
+    {
+    	std::stringstream ss;
+    	ss << "cudaGetDeviceCount failed: " << cudaGetErrorString(status) <<  '\n';
+        BOOST_ERROR(ss.str().c_str());
+    }
+    if (num_devices == 0) {
+        std::cerr << "No devices - can't run this test\n";
+        return boost::report_errors();
+    }
+	// TODO: Test basic_cstr_writer's with different character types
+	test_cstr_writer();
 
-    // Reset the device and exit
-    // cudaDeviceReset causes the driver to clean up all state. While
-    // not mandatory in normal operation, it is good practice.  It is also
-    // needed to ensure correct operation when the application is being
-    // profiled. Calling cudaDeviceReset causes all profile data to be
-    // flushed before the application exits
     cudaDeviceReset();
-    return EXIT_SUCCESS;
+    return boost::report_errors();
 }
-
