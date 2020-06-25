16bf013de85d1fafee024c4c9c327ada3610f226
Roberto <robhz786@gmail.com>
2020-06-25 18:23:11 -0300

    Use --std=c++2a on compilation benchmarks.

--- benchmarks/compilation/to_string_fmtlib.cpp
+++ benchmarks/compilation/to_string_fmtlib.cpp
@@ -3,10 +3,10 @@
 
 void FUNCTION_NAME (std::string& out)
 {
-    out += fmt::format("{}", ARG(0));                                                        
-    out += fmt::format("{}{}", ARG(1), ARG(2));                                              
-    out += fmt::format("{}{}{}", ARG(3), ARG(4), ARG(5));                                    
-    out += fmt::format("{}{}{}{}", ARG(6), ARG(7), ARG(8), ARG(9));                          
-    out += fmt::format("{}{}{}{}{}", ARG(10), ARG(11), ARG(12), ARG(13), ARG(14));           
+    out += fmt::format("{}", ARG(0));
+    out += fmt::format("{}{}", ARG(1), ARG(2));
+    out += fmt::format("{}{}{}", ARG(3), ARG(4), ARG(5));
+    out += fmt::format("{}{}{}{}", ARG(6), ARG(7), ARG(8), ARG(9));
+    out += fmt::format("{}{}{}{}{}", ARG(10), ARG(11), ARG(12), ARG(13), ARG(14));
     out += fmt::format("{}{}{}{}{}{}", ARG(15), ARG(16), ARG(17), ARG(18), ARG(19), ARG(20));
 }
