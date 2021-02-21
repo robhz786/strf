2a4e6e72866f970d8ff7521ba4db17905f1a12d8
Roberto <robhz786@gmail.com>
2021-02-21 20:43:53 -0300

    Redefine `to_string` as a an instance of `destination_no_reserve`.
    Which means that now the size of the content
    to be printed is not pre-calculated.

--- include/strf/to_string.hpp
+++ include/strf/to_string.hpp
@@ -267,7 +267,7 @@ public:
 template <typename CharT, typename Traits, typename Allocator>
 inline auto append(std::basic_string<CharT, Traits, Allocator>& str)
 {
-    return strf::destination_calc_size
+    return strf::destination_no_reserve
         < strf::detail::basic_string_appender_creator<CharT, Traits, Allocator> >
         { str };
 }
@@ -282,31 +282,31 @@ inline auto assign(std::basic_string<CharT, Traits, Allocator>& str)
 template< typename CharT
         , typename Traits = std::char_traits<CharT>
         , typename Allocator = std::allocator<CharT> >
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<CharT, Traits, Allocator> >
     to_basic_string{};
 
 #if defined(__cpp_char8_t)
 
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<char8_t> >
     to_u8string{};
 
 #endif
 
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<char> >
     to_string{};
 
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<char16_t> >
     to_u16string{};
 
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<char32_t> >
     to_u32string{};
 
-constexpr strf::destination_calc_size
+constexpr strf::destination_no_reserve
     < strf::detail::basic_string_maker_creator<wchar_t> >
     to_wstring{};
 
