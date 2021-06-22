2fcf4eedd6af6c0c0f80d7322a6e41a9f4911304
Roberto <robhz786@gmail.com>
2021-06-22 13:58:44 -0300

    Edit codecov build on CI: apply -fno-exceptions and -fno-inline
    To eliminate false positives in the branch coverage.

--- ci/travis/codecov_build.sh
+++ ci/travis/codecov_build.sh
@@ -9,13 +9,15 @@ mkdir cmake_build
 cd cmake_build
 
 script_dir="$( pwd -P )"
+cflags="-O0 -Wall -Wextra -fno-exceptions -fno-inline -fprofile-arcs -ftest-coverage -DNDEBUG"
+cxxflags="$cflags"
 
 cmake -DSTRF_BUILD_TESTS=ON \
       -DSTRF_BUILD_EXAMPLES=ON \
       -DCMAKE_CXX_COMPILER=g++-7 \
       -DCMAKE_C_COMPILER=gcc-7 \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -O0 -fprofile-arcs -ftest-coverage -DNDEBUG" \
-      -DCMAKE_C_FLAGS="-Wall -Wextra -O0 -fprofile-arcs -ftest-coverage -DNDEBUG" \
+      -DCMAKE_CXX_FLAGS="$cflags" \
+      -DCMAKE_C_FLAGS="$cxxflags" \
       -DCMAKE_EXE_LINKER_FLAGS="-ftest-coverage -fprofile-arcs" \
       -DCMAKE_CXX_STANDARD=$CXX_STANDARD \
       -DCMAKE_BUILD_TYPE=Debug \
