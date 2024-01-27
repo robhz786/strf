41c4d6d26af65080504c5a3d9fbd8eadc509ecb9
Roberto Hinz <robhz786@gmail.com>
2024-01-27 12:15:33 -0400

    Modify one of the constructors in printable_with_fmt
    The one that let you pass arguments to initialize one of the
    bases, while the others are initialized from another
    printable_with_fmt object.

--- include/strf/detail/printable_with_fmt.hpp
+++ include/strf/detail/printable_with_fmt.hpp
@@ -10,7 +10,190 @@
 
 namespace strf {
 
+template <typename PrintingTraits, class... Fmts>
+class printable_with_fmt;
+
 namespace detail {
+
+template <typename SeparatorFmt, typename FmtsPairListBefore, typename FmtsPairListAfter>
+struct separate_fmtpair_list_impl;
+
+template <
+    typename SeparatorFmt,
+    typename... PairsBefore,
+    typename    FirstFmtInFirstPairAfter,
+    typename... OtherPairsAfter >
+struct separate_fmtpair_list_impl
+    < SeparatorFmt
+    , mp_type_list<PairsBefore...>
+    , mp_type_list
+        < mp_type_pair<FirstFmtInFirstPairAfter, SeparatorFmt>
+        , OtherPairsAfter... > >
+{
+    using pairs_before = mp_type_list<PairsBefore...>;
+    using pairs_after  = mp_type_list<OtherPairsAfter...>;
+    using separator_pair = mp_type_pair<FirstFmtInFirstPairAfter, SeparatorFmt>;
+    using separator_fmt = SeparatorFmt;
+    constexpr static bool found = true;
+};
+
+template <typename SeparatorFmt, typename... PairsBefore>
+struct separate_fmtpair_list_impl
+    < SeparatorFmt
+    , mp_type_list<PairsBefore...>
+    , mp_type_list<> >
+{
+    using pairs_before = mp_type_list<PairsBefore...>;
+    using pairs_after  = mp_type_list<>;
+    using separator_pair = void;
+    using separator_fmt = void;
+    constexpr static bool found = false;
+};
+
+template
+    < typename SeparatorFmt
+    , typename... PairsBefore
+    , typename    FirstFmtInFirstPairAfter
+    , typename    SecondFmtInFirstPairAfter
+    , typename... OtherPairsAfter >
+struct separate_fmtpair_list_impl
+    < SeparatorFmt
+    , mp_type_list<PairsBefore...>
+    , mp_type_list
+        < mp_type_pair<FirstFmtInFirstPairAfter, SecondFmtInFirstPairAfter>
+        , OtherPairsAfter... > >
+{
+    using this_pair = mp_type_pair<FirstFmtInFirstPairAfter, SecondFmtInFirstPairAfter>;
+
+    using next_pairs_list_before =
+        typename mp_type_list<PairsBefore...>::template add_back <this_pair>;
+
+    using next_pairs_list_after = mp_type_list<OtherPairsAfter...>;
+
+    using next_impl = separate_fmtpair_list_impl
+        < SeparatorFmt
+        , next_pairs_list_before
+        , next_pairs_list_after >;
+
+    using pairs_before   = typename next_impl::pairs_before;
+    using pairs_after    = typename next_impl::pairs_after;
+    using separator_pair = typename next_impl::separator_pair;
+    using separator_fmt  = typename next_impl::separator_fmt;
+    constexpr static bool found = next_impl::found;
+};
+
+template <typename SeparatorFmt, typename FmtsPairsList>
+struct separate_fmtpair_list: separate_fmtpair_list_impl
+    < SeparatorFmt, mp_type_list<>, FmtsPairsList >
+{
+};
+
+template <typename PairsList>
+struct transpose_fmt_list;
+
+template <typename... Pairs>
+struct transpose_fmt_list<mp_type_list<Pairs...>>
+{
+    using list_of_firsts  = mp_type_list<typename Pairs::first ...>;
+    using list_of_seconds = mp_type_list<typename Pairs::second ...>;
+};
+
+template <typename PrintableWithFmt, typename FmtList>
+struct get_list_of_fmtfn_bases_impl;
+
+template <typename PrintableWithFmt, typename... Fmts>
+struct get_list_of_fmtfn_bases_impl<PrintableWithFmt, mp_type_list<Fmts...>>
+{
+    using type = mp_type_list<typename Fmts::template fn<PrintableWithFmt>...>;
+};
+
+template <typename PrintableWithFmt, typename FmtList>
+using get_list_of_fmtfn_bases =
+    typename get_list_of_fmtfn_bases_impl<PrintableWithFmt, FmtList>::type;
+
+template <typename SrcList, typename DstList>
+struct each_dst_is_contructible_from_cref_of_each_src_impl;
+
+template <typename SrcList, typename DstList>
+struct each_dst_is_contructible_from_rval_ref_of_each_src_impl;
+
+template <typename... Src, typename... Dst>
+struct each_dst_is_contructible_from_cref_of_each_src_impl
+    < mp_type_list<Src...>, mp_type_list<Dst...> >
+{
+    static constexpr bool value =
+        fold_and< std::is_constructible<Src, const Dst&>::value... >::value;
+};
+
+template <typename... Src, typename... Dst>
+struct each_dst_is_contructible_from_rval_ref_of_each_src_impl
+    < mp_type_list<Src...>, mp_type_list<Dst...> >
+{
+    static constexpr bool value =
+        fold_and< std::is_constructible<Src, detail::remove_cvref_t<Dst> >::value... >::value;
+};
+
+template <typename SrcList, typename DstList>
+constexpr bool each_dst_is_contructible_from_cref_of_each_src =
+    each_dst_is_contructible_from_cref_of_each_src_impl<SrcList, DstList>::value;
+
+template <typename SrcList, typename DstList>
+constexpr bool each_dst_is_contructible_from_rval_ref_of_each_src =
+    each_dst_is_contructible_from_rval_ref_of_each_src_impl<SrcList, DstList>::value;
+
+template
+    < bool Enabled
+    , typename SeparatorDstFmt
+    , typename SrcPrintableWithFmt
+    , typename DstPrintableWithFmt >
+struct separate_fmts_of_two_printable_with_fmt;
+
+template
+    < typename SeparatorDstFmt
+    , typename SrcPrintableTraits
+    , typename... SrcFmts
+    , typename DstPrintableTraits
+    , typename... DstFmts >
+struct separate_fmts_of_two_printable_with_fmt
+    < true
+    , SeparatorDstFmt
+    , printable_with_fmt<SrcPrintableTraits, SrcFmts...>
+    , printable_with_fmt<DstPrintableTraits, DstFmts...> >
+{
+    static_assert(sizeof...(SrcFmts) == sizeof...(DstFmts), "");
+
+    using pairs_list = mp_type_list<mp_type_pair<SrcFmts, DstFmts>...>;
+    using impl = separate_fmtpair_list<SeparatorDstFmt, pairs_list>;
+
+    using before = transpose_fmt_list< typename impl::pairs_before >;
+    using after  = transpose_fmt_list< typename impl::pairs_after >;
+
+    using src_fmts_before = typename before::list_of_firsts;
+    using dst_fmts_before = typename before::list_of_seconds;
+
+    using src_fmts_after = typename after::list_of_firsts;
+    using dst_fmts_after = typename after::list_of_seconds;
+
+    constexpr static bool found_selected_fmt_in_dst_list = impl::found;
+
+    using src_printable_with_fmt = printable_with_fmt<SrcPrintableTraits, SrcFmts...>;
+    using dst_printable_with_fmt = printable_with_fmt<DstPrintableTraits, DstFmts...>;
+
+    using src_fmtfn_bases_before = get_list_of_fmtfn_bases
+        < src_printable_with_fmt, src_fmts_before >;
+
+    using dst_fmtfn_bases_before = get_list_of_fmtfn_bases
+        < dst_printable_with_fmt, dst_fmts_before >;
+
+    using src_fmtfn_bases_after = get_list_of_fmtfn_bases
+        < src_printable_with_fmt, src_fmts_after>;
+
+    using dst_fmtfn_bases_after = get_list_of_fmtfn_bases
+        < dst_printable_with_fmt, dst_fmts_after>;
+
+    using separator_fmtfn_base = typename
+        SeparatorDstFmt::template fn<dst_printable_with_fmt>;
+};
 
 template
     < class From
@@ -40,51 +223,12 @@ struct fmt_replace_impl<From, List<T...> >
             < From, To, List, T...>::type;
 };
 
-template <typename FmtA, typename FmtB, typename ValueWithFormat>
-struct fmt_forward_switcher
-{
-    template <typename FmtAInit>
-    static STRF_HD const typename FmtB::template fn<ValueWithFormat>&
-    f(const FmtAInit&, const ValueWithFormat& v)
-    {
-        return v;
-    }
-
-    template <typename FmtAInit>
-    static STRF_HD typename FmtB::template fn<ValueWithFormat>&&
-    f(const FmtAInit&, ValueWithFormat&& v)
-    {
-        return std::move(v);
-    }
-};
-
-template <typename FmtA, typename ValueWithFormat>
-struct fmt_forward_switcher<FmtA, FmtA, ValueWithFormat>
-{
-    template <typename FmtAInit>
-    static constexpr STRF_HD FmtAInit&&
-    f(strf::detail::remove_reference_t<FmtAInit>& fa,  const ValueWithFormat&)
-    {
-        return static_cast<FmtAInit&&>(fa);
-    }
-
-    template <typename FmtAInit>
-    static constexpr STRF_HD FmtAInit&&
-    f(strf::detail::remove_reference_t<FmtAInit>&& fa, const ValueWithFormat&)
-    {
-        return std::move(fa);
-    }
-};
-
 } // namespace detail
 
 template <typename List, typename From, typename To>
 using fmt_replace
     = typename strf::detail::fmt_replace_impl<From, List>
     ::template type_tmpl<To>;
-
-template <typename PrintingTraits, class... Fmts>
-class printable_with_fmt;
 
 template <typename PrintingTraits, class... Fmts>
 using value_with_formatters
@@ -222,21 +366,85 @@ public:
     {
     }
 
-    template <typename Fmt, typename FmtInit, typename ... OtherFmts>
+    template
+        < typename SrcPrintingTraits
+        , typename... SrcFmts
+        , typename SelectedFmt
+        , typename... SelectedFmtInitArgs
+        , typename Helper = detail::separate_fmts_of_two_printable_with_fmt
+             < sizeof...(SrcFmts) == sizeof...(Fmts)
+             , SelectedFmt
+             , printable_with_fmt<SrcPrintingTraits, SrcFmts...>
+             , printable_with_fmt<PrintingTraits, Fmts...> >
+
+        , detail::enable_if_t<Helper::found_selected_fmt_in_dst_list, int> = 0
+
+        , detail::enable_if_t
+            < std::is_constructible
+                < value_type, const typename SrcPrintingTraits::forwarded_type&>::value
+            , int > = 0
+
+        , detail::enable_if_t
+            < detail::each_dst_is_contructible_from_cref_of_each_src
+                < typename Helper::src_fmtfn_bases_before
+                , typename Helper::dst_fmtfn_bases_before >
+            , int > = 0
+
+        , detail::enable_if_t
+            < detail::each_dst_is_contructible_from_cref_of_each_src
+                 < typename Helper::src_fmtfn_bases_after
+                 , typename Helper::dst_fmtfn_bases_after >
+            , int > = 0
+
+        , detail::enable_if_t
+            < std::is_constructible
+                  < typename Helper::separator_fmtfn_base
+                  , SelectedFmtInitArgs... > ::value
+            , int > = 0 >
+
     constexpr STRF_HD printable_with_fmt
-        ( const strf::printable_with_fmt<PrintingTraits, OtherFmts...>& f
-        , strf::tag<Fmt>
-        , FmtInit&& fmt_init )
-        : Fmts::template fn<printable_with_fmt<PrintingTraits, Fmts...>>
-            ( strf::detail::fmt_forward_switcher
-                  < Fmt
-                  , Fmts
-                  , strf::printable_with_fmt<PrintingTraits, OtherFmts...> >
-              :: template f<FmtInit>(fmt_init, f) )
-            ...
-        , value_(f.value())
+        ( const strf::printable_with_fmt<SrcPrintingTraits, SrcFmts...>& src
+        , strf::tag<SelectedFmt>
+        , SelectedFmtInitArgs&&... args )
+        : printable_with_fmt
+            ( detail::mp_type_list
+                < typename Helper::src_fmtfn_bases_before
+                , typename Helper::dst_fmtfn_bases_before
+                , typename Helper::src_fmtfn_bases_after
+                , typename Helper::dst_fmtfn_bases_after
+                , typename Helper::separator_fmtfn_base > ()
+            , src
+            , std::forward<SelectedFmtInitArgs&&>(args)... )
     {
     }
+
+private:
+
+    template
+        < typename... SrcBasesBefore
+        , typename... DstBasesBefore
+        , typename... SrcBasesAfter
+        , typename... DstBasesAfter
+        , typename SrcPrintableWithFmt
+        , typename SelectedBase
+        , typename... Args >
+    constexpr STRF_HD printable_with_fmt
+        ( detail::mp_type_list
+            < detail::mp_type_list<SrcBasesBefore...>
+            , detail::mp_type_list<DstBasesBefore...>
+            , detail::mp_type_list<SrcBasesAfter...>
+            , detail::mp_type_list<DstBasesAfter...>
+            , SelectedBase >
+        , const SrcPrintableWithFmt& src
+        , Args&&... args )
+        : DstBasesBefore(static_cast<const SrcBasesBefore&>(src))...
+        , SelectedBase(std::forward<Args>(args)...)
+        , DstBasesAfter(static_cast<const SrcBasesAfter&>(src))...
+        , value_(src.value())
+    {
+    }
+
+public:
 
     constexpr STRF_HD const value_type& value() const
     {
